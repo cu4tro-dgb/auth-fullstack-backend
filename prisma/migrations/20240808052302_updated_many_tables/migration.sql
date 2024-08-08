26269e71e6492b600eb26a197866752c517130b5
cu4tr0-dgb <saekaronw@gmail.com>
2024-08-08 08:25:54 -0500

    change schema prisma

---
+++ prisma/migrations/20240808052302_updated_many_tables/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[roleId,permissionId]` on the table `RolePermission` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId,roleId]` on the table `UserRole` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Permission" ADD COLUMN     "description" TEXT;
+
+-- AlterTable
+ALTER TABLE "Role" ADD COLUMN     "description" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RolePermission_roleId_permissionId_key" ON "RolePermission"("roleId", "permissionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRole_userId_roleId_key" ON "UserRole"("userId", "roleId");
